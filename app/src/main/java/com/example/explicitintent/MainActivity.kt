ac0ca5beae01ccecdff55007959a8390e89d9716
Viraj Gund <virajgund31@gmail.com>
2023-02-07 10:41:31 +0530

    adding file of the project

---
+++ app/src/main/java/com/example/explicitintent/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.explicitintent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener {
+
+            intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
